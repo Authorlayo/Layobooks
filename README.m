b2e747083242dad647d2bd66635788251deaba55
Authorlayo <142248304+Authorlayo@users.noreply.github.com>
2023-08-14 17:27:28 +0100

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,31 @@
+# Layobook
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book Collection</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>My Book Collection</h1>
+    </header>
+    <main>
+        <section class="book">
+            <img src="book1.jpg" alt="Book 1">
+            <h2>Book Title 1</h2>
+            <p>Author: Author Name</p>
+        </section>
+        <section class="book">
+            <img src="book2.jpg" alt="Book 2">
+            <h2>Book Title 2</h2>
+            <p>Author: Author Name</p>
+        </section>
+        <!-- Add more book sections as needed -->
+    </main>
+    <footer>
+        <p>&copy; 2023 Book Collection. All rights reserved.</p>
+    </footer>
+</body>
+</html>
